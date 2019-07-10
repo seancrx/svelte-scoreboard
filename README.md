c8c67ab7b51cb880b6b0e8433cd69129cd9a746f
seancrx <crongxiang7@gmail.com>
2019-07-10 17:22:34 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Test Svelte App
+
+This is a project for testing out svelte js, it has no any purpose other than let me test out svelte js.
+
+The project follows Brad Traversy tutorial on YouTube at [Svelte Crash Course](https://youtu.be/uK2RnIzrQ0M).
---
+++ src/Player.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+	export let name;
+	export let points;
+	let showControls = false;
+
+	const addPoint = () => (points++);
+	const removePoint = () => (points--);
+    const toogleControl = () => (showControls = !showControls);
+    const deletePlayer = () => dispatch("deleteplayer", name);
+</script>
+
+<style>
+	h1 {
+		color: #204f6e;
+	}
+	h3 {
+		margin-bottom: 10px;
+	}
+</style>
+
+	<div class="card">
+        <h1>{name}</h1>
+        <button class="btn btn-danger btn-sm" on:click={deletePlayer}>x</button>
+		<h3>Points: {points}</h3>
+		<button class="btn btn-sm" on:click={toogleControl}>{#if showControls}-{:else}+{/if}</button>
+		{#if showControls}
+			<button class="btn" on:click={addPoint}>+1</button>
+			<button class="btn btn-dark" on:click={removePoint}>-1</button>
+			<input type="number" bind:value={points}>
+		{/if}
+	</div>
